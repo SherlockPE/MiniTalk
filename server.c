9286eb684dd3a40e314a9f01980fee46593d81e5
Fabricio_lopez <flopez-r@student.42madrid.com>
2023-12-06 16:09:56 +0100

    update

---
+++ server.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flopez-r <flopez-r@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/06 16:01:03 by flopez-r          #+#    #+#             */
+/*   Updated: 2023/12/06 16:09:26 by flopez-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	main(int argc, char const *argv[])
+{
+	int		PID;
+	char	*message;
+
+	PID = atoi(argv[1]);
+	message = argv[2];
+
+	//Codigo ara enviar el mensaje al PID
+
+	return (0);
+}
