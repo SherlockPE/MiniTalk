e09c1e9d23b0873f07266a578b750ba6746b629d
Fabricio_lopez <flopez-r@student.42madrid.com>
2024-01-06 15:21:43 +0100

    Actualizacion de la mañana

--- src/client.c
+++ src/client.c
@@ -6,22 +6,67 @@
 /*   By: flopez-r <flopez-r@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 15:50:18 by flopez-r          #+#    #+#             */
-/*   Updated: 2024/01/04 17:10:11 by flopez-r         ###   ########.fr       */
+/*   Updated: 2024/01/06 14:51:41 by flopez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+void	ft_pow(int *base, int exponent)
+{
+	if (base == 0 && exponent == 0)
+	{
+		*base = 0;
+		return ;	
+	}
+	else if (exponent == 0)
+	{
+		*base = 1;
+		return ;
+	}
+	while (exponent--)
+		*base *= *base;
+}
+
+void	convert_to_binary(int	base)
+{
+	int	exponent;
+	
+	exponent = 7;
+	while (base) 
+	{
+		/* code */
+	}
+}
+
+
 int	main(int argc, char const *argv[])
 {
-	int		PID;
+	pid_t PID;
 	char	*message;
-
 	if (argc == 1)
-		return (0);
-	
+		return (ft_printf("Argument not valid\n"), 0);
+
+	//Receive parameters
 	PID = ft_atoi(argv[1]);
 	message = (char *)argv[2];
 
-	ft_printf("%s", message);
+	//Sending message
+	int i = 0;
+	while (message[i])
+	{
+		convert_to_binary(message[i]);
+	}
+
 }
+
+// int signal = ft_atoi(argv[2]);
+
+// if (signal == 0)
+// 	kill(PID, SIGINT);
+// else if (signal == 1)
+// 	kill(PID, SIGSTOP);
+// else if (signal == 2)
+// 	kill(PID, SIGCONT);
+// else
+// 	ft_printf("Argument not valid\n");
--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: flopez-r <flopez-r@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 16:01:03 by flopez-r          #+#    #+#             */
-/*   Updated: 2024/01/05 18:38:35 by flopez-r         ###   ########.fr       */
+/*   Updated: 2024/01/06 13:55:36 by flopez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,13 @@ void	controlador(int	signal)
 	ft_printf("\nSeñal recibida: %d\n", signal);
 }
 
+void	handler(int signal)
+{
+	if (signal == SIGUSR1)
+		ft_printf("1\n");
+	else
+		ft_printf("0\n");
+}
 
 int	main(void)
 {
@@ -27,17 +34,25 @@ int	main(void)
 
 	ft_printf("MINITALK PRUEBA\n");
 
-	int i = 1;
-	while (i)
+	signal(SIGUSR1, handler);
+	signal(SIGUSR2, handler);
+
+	while (1)
 	{
-		int j = 10;
-		while (j--)
-		{
-			ft_printf("%d ", i++);
-			sleep(1);
-		}
-		ft_printf("\n");
 	}
+	
+
+	// int i = 1;
+	// while (i)
+	// {
+	// 	int j = 10;
+	// 	while (j--)
+	// 	{
+	// 		ft_printf("%d ", i++);
+	// 		sleep(1);
+	// 	}
+	// 	ft_printf("\n");
+	// }
 
 	return (0);
 }
