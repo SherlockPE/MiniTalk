70f85a40a4c44f9ed7455d9004031450d2645454
Fabricio_lopez <flopez-r@student.42madrid.com>
2024-01-12 18:50:47 +0100

    vamoo

--- src/bonus_client.c
+++ src/bonus_client.c
@@ -6,7 +6,7 @@
 /*   By: flopez-r <flopez-r@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 15:50:18 by flopez-r          #+#    #+#             */
-/*   Updated: 2024/01/12 16:06:09 by flopez-r         ###   ########.fr       */
+/*   Updated: 2024/01/12 18:09:26 by flopez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,49 +51,3 @@ int	main(int argc, char *argv[])
 		i++;
 	}
 }
-
-// void	convert_to_binary(int number, int *binary_number)
-// {
-// 	int	add_units;
-
-// 	add_units = 1;
-// 	while (number > 0)
-// 	{
-// 		*binary_number += (number % 2) * add_units;
-// 		number /= 2;
-// 		add_units *= 10;
-// 	}
-// }
-
-// void	send_caracter(pid_t PID, int	binary_number)
-// {
-// 	int	i;
-// 	char	*message;
-
-// 	(void)PID;
-// 	message = ft_itoa(binary_number);
-// 	//PROTEJER ESTE MENSAJE
-// 	ft_printf("%s\n", message);
-// 	i = 0;
-// 	while (message[i])
-// 	{
-// 		if (message[i] == '1')
-// 			kill(PID, SIGUSR1);
-// 		else if (message[i] == '0')
-// 			kill(PID, SIGUSR2);
-// 		usleep(10);
-// 		i++;
-// 	}
-// 	free(message);
-// }
-
-// int signal = ft_atoi(argv[2]);
-
-// if (signal == 0)
-// 	kill(PID, SIGINT);
-// else if (signal == 1)
-// 	kill(PID, SIGSTOP);
-// else if (signal == 2)
-// 	kill(PID, SIGCONT);
-// else
-// 	ft_printf("Argument not valid\n");
--- src/bonus_server.c
+++ src/bonus_server.c
@@ -6,35 +6,16 @@
 /*   By: flopez-r <flopez-r@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 16:01:03 by flopez-r          #+#    #+#             */
-/*   Updated: 2024/01/12 16:16:01 by flopez-r         ###   ########.fr       */
+/*   Updated: 2024/01/12 18:08:48 by flopez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-/* void	handler(int signal)
-{
-	static	int	character;
-	static	int	num_bits;
-
-	character = 0;
-	num_bits = 0;
-	if (signal == SIGUSR1)
-		character += (1 << num_bits);
-	num_bits++;
-	if (num_bits == 8)
-	{
-		// if (character == 0)
-		// 	kill();
-		write(1, &character, 1);
-		character = 0;
-		num_bits = 0;
-	}
-} */
 void	handler_bonus(int signal, siginfo_t *info, void *algo)
 {
-	static int	character = 0;
-	static int	num_bits = 0;
+	static int	character;
+	static int	num_bits;
 
 	(void)algo;
 	if (signal == SIGUSR1)
